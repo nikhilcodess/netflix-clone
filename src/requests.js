ba740f403a0a7b5e4df1a88da901ad5cd76c67b5
nikhilcodess <nikhilkaushal0000@gmail.com>
2021-06-15 16:15:42 +0530

    Completed Clone

--- src/requests.js
+++ src/requests.js
@@ -2,13 +2,13 @@ const API_KEY = "a332e55334ef64e4480bddc9097e9750";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchedTopRated: `/movie/top_rated?api_key=${API_KEY}&lanuage=en-US`,
-  fetchedActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchedComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchedHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchedRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchedDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
 
 export default requests;
